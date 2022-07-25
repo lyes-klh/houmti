64fa9347b8d1210d679ffaa4a345041e5b0e4262
Lyes Kellouche <lyeskellouche@gmail.com>
2022-07-25 22:06:29 +0100

    bug fix

--- server/routes/neighborhoodsRouter.js
+++ server/routes/neighborhoodsRouter.js
@@ -10,9 +10,9 @@ const {
 
 const router = express.Router();
 
-router.use(protect);
 router.route('/').get(getAllNeighborhoods);
 
+router.use(protect);
 router.use(restrictToAdmin);
 router.route('/').post(createNeighborhood);
 router.route('/:id').patch(updateNeighborhood).delete(deleteNeighborhood);
