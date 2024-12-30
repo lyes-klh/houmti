1796659c9731c1492b9d88a67d35098bfcf55ef8
Lyes Kellouche <lyeskellouche@gmail.com>
2024-12-30 23:26:40 +0100

    fix city model constraints

--- server/models/cityModel.js
+++ server/models/cityModel.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const citySchema = new mongoose.Schema({
   cityName: {
     type: String,
-    required: [true, 'City must have a name'],
+    required: [true, "City must have a name"],
   },
 
   zipCode: {
@@ -13,20 +13,18 @@ const citySchema = new mongoose.Schema({
 
   countryName: {
     type: String,
-    required: [true, 'City must belong to a country'],
+    required: [true, "City must belong to a country"],
   },
 
   countryISOCode: {
     type: String,
-    required: [true, 'Country must have an ISO code'],
-    unique: true,
+    required: [true, "Country must have an ISO code"],
   },
 
   countryCode: {
     type: String,
-    required: [true, 'Country must have a phone code'],
-    unique: true,
+    required: [true, "Country must have a phone code"],
   },
 });
 
-module.exports = mongoose.model('City', citySchema);
+module.exports = mongoose.model("City", citySchema);
